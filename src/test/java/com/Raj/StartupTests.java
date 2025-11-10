6735bbab28be01579357437b9f282dc40dc0b4a2
Vitor Silva <vitorsilva3579@gmail.com>
2025-11-09 22:05:57 -0300

    Fist Steps in microservices

---
+++ src/test/java/com/Raj/StartupTests.java
@@ -0,0 +1,13 @@
+package com.Raj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
